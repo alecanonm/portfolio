1ed32babf30b6cc91611af0608c42c6647ea13fc
alecanonm <al3jandrocan0n@gmail.com>
2023-09-19 21:12:39 -0500

    the section my tools has been completed

---
+++ portfolio/src/components/Badge.jsx
@@ -0,0 +1,16 @@
+const Badge = ({ img, name }) => {
+  return (
+    <section className="flex  flex-col items-center w-40 pb-5">
+      <section className=" bg-[#0f204a] rounded-md p-2">
+        <figure className=" flex items-center w-24 h-24 p-4 rounded-full bg-white hover:scale-105 transition-transform duration-300 ease-in-out">
+          <img src={img} className="rounded-xl" alt={`badge-${name}`} />
+        </figure>
+      </section>
+      <section className=" relative top-1 bg-black w-24 text-center rounded text-white font-semibold">
+        {name}
+      </section>
+    </section>
+  );
+};
+
+export default Badge;
