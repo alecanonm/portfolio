36afb74fb8ad0807d464cfcf9ea25a6b4298c02a
alecanonm <al3jandrocan0n@gmail.com>
2023-09-19 22:03:07 -0500

    the pagination has been added

--- portfolio/src/components/Nav.tsx
+++ portfolio/src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useMediaQuery } from "../utils/useMediaQuery";
 
 const navMotion = {
